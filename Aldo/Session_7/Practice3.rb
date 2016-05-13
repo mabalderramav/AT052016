95640e21c15550a86490b9d41177deea70ea12d8
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2016-05-13 17:32:50 -0400

    handling all forms of arrays, done

--- Aldo/Session_7/Practice3.rb
+++ Aldo/Session_7/Practice3.rb
@@ -4,9 +4,13 @@ handling all forms of arrays
 	
 =end
 require_relative "library/RubyArrays"
+#Method one
 v1, v2, v3 = RubyArrays.setThreeArrays
 RubyArrays.getThreeArrays v1,v2,v3
+#Method two
 RubyArrays.unionTwoArray v1,v3
+#Method three
 v4,v5 = RubyArrays.joinTwoArray v1,v3
+#Method four
 RubyArrays.getNewArray v4
 RubyArrays.getNewArray v5
